279812c5a0c3e723266c03f34cab86cc9e9def9d
erwincoumans <erwin.coumans@gmail.com>
2017-11-04 13:23:11 -0700

    move botlab files around

--- botlab.py
+++ botlab.py
@@ -1,11 +1,19 @@
 import pybullet as p
-p.connect(p.GUI)
+
+cid = p.connect(p.SHARED_MEMORY)
+if (cid<0):
+	p.connect(p.GUI)
+	
+p.resetSimulation()
+#waitId = p.addUserDebugText(text="Wait while loading SDF",textPosition=[0,0,0],textColorRGB=[1,0,0],textSize=10)
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,0)
 p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER,0)
 objs = p.loadSDF("botlab/botlab.sdf")
 
 zero=[0,0,0]
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING,1)
+
+#p.removeUserDebugItem(waitId)
 
 print("converting y to z axis")
 for o in objs:
