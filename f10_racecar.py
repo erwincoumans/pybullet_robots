6944c87da85852972b4c17eb49c69a65cd1d5950
erwincoumans <erwin.coumans@gmail.com>
2018-06-01 22:40:39 -0700

    add the actual racecar prototype

--- f10_racecar.py
+++ f10_racecar.py
@@ -1,9 +1,69 @@
 import pybullet as p
 import time
 p.connect(p.GUI)
-#p.loadSDF("f10_racecar/barca_track/barca_track.sdf")
-p.loadSDF("f10_racecar/meshes/barca_track.sdf", globalScaling=1)
-p.loadURDF("f10_racecar/racecar_differential.urdf", [0,0,1])
-while (1):
-	p.stepSimulation()
+
+p.resetSimulation()
+p.setGravity(0,0,-10)
+useRealTimeSim = 1
+
+p.setRealTimeSimulation(useRealTimeSim) # either this
+
+track = p.loadSDF("f10_racecar/meshes/barca_track.sdf", globalScaling=1)
+car = p.loadURDF("f10_racecar/racecar_differential.urdf", [0,0,1])
+
+
+	
+for wheel in range(p.getNumJoints(car)):
+	p.setJointMotorControl2(car,wheel,p.VELOCITY_CONTROL,targetVelocity=0,force=0)
+	p.getJointInfo(car,wheel)	
+
+wheels = [8,15]
+print("----------------")
+
+#p.setJointMotorControl2(car,10,p.VELOCITY_CONTROL,targetVelocity=1,force=10)
+c = p.createConstraint(car,9,car,11,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=1, maxForce=10000)
+
+c = p.createConstraint(car,10,car,13,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+
+c = p.createConstraint(car,9,car,13,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+
+c = p.createConstraint(car,16,car,18,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=1, maxForce=10000)
+
+
+c = p.createConstraint(car,16,car,19,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+
+c = p.createConstraint(car,17,car,19,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, maxForce=10000)
+
+c = p.createConstraint(car,1,car,18,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, gearAuxLink = 15, maxForce=10000)
+c = p.createConstraint(car,3,car,19,jointType=p.JOINT_GEAR,jointAxis =[0,1,0],parentFramePosition=[0,0,0],childFramePosition=[0,0,0])
+p.changeConstraint(c,gearRatio=-1, gearAuxLink = 15,maxForce=10000)
+
+
+steering = [0,2]
+
+targetVelocitySlider = p.addUserDebugParameter("wheelVelocity",-50,50,0)
+maxForceSlider = p.addUserDebugParameter("maxForce",0,50,20)
+steeringSlider = p.addUserDebugParameter("steering",-1,1,0)
+while (True):
+	maxForce = p.readUserDebugParameter(maxForceSlider)
+	targetVelocity = p.readUserDebugParameter(targetVelocitySlider)
+	steeringAngle = p.readUserDebugParameter(steeringSlider)
+	#print(targetVelocity)
+	
+	for wheel in wheels:
+		p.setJointMotorControl2(car,wheel,p.VELOCITY_CONTROL,targetVelocity=targetVelocity,force=maxForce)
+		
+	for steer in steering:
+		p.setJointMotorControl2(car,steer,p.POSITION_CONTROL,targetPosition=-steeringAngle)
+		
+	steering
+	if (useRealTimeSim==0):
+		p.stepSimulation()
 	time.sleep(1./240.)
